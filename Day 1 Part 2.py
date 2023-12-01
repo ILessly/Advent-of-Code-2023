25e1f4cd1853f8278467464884c7499f614137d8
ILessly <87824224+ILessly@users.noreply.github.com>
2023-12-01 13:43:11 -0600

    Create Day 1 Part 2.py

---
+++ Day 1 Part 2.py
@@ -0,0 +1,24 @@
+#Advent of Code Day 1 part 2
+
+file = open(r"filepathonmyhomecomputerlol", "r")
+inputlist = file.readlines()
+i=0
+t=0
+total=0
+
+digitltrs = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+digitnums = ['o1n', 't2o', 't3e', 'f4r', 'f5e', 's6x', 's7n', 'e8t', 'n9e']
+
+for i in inputlist:
+  i=i.strip()
+  for t in digitltrs:
+    if t in i:
+      i = i.replace(t, digitnums[digitltrs.index(t)])
+      
+  number = i.translate({ord(a): None for a in 'abcdefghijklmnopqrstuvwxyz'})
+  x1 = number[0:1]
+  x2 = number[-1:]
+  value = int(x1 +x2)
+  total = total + value
+  
+print(total)
