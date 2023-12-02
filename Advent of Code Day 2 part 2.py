be22361670cbd66f464edf08d81ced373152b572
ILessly <87824224+ILessly@users.noreply.github.com>
2023-12-02 12:10:25 -0600

    Add files via upload

---
+++ Advent of Code Day 2 part 2.py
@@ -0,0 +1,32 @@
+#Advent of Code Day 2
+
+input = open(r"C:\Users\chimp\Desktop\Advent of Code\2023\Day 2 Input.txt", "r")
+inputlist = input.readlines()
+#print(inputlist)
+
+total = 0
+
+for i in inputlist:        #Isolate each game
+    i = i.strip()
+    i = i.split(":")
+    ID = i[0]               #Split the game ID from results
+    i = i[1]
+    ID = int(ID[5:])
+    r = 0
+    g = 0
+    b = 0
+    gameset = i.split(";")
+    for group in gameset:           #Isolate each grab
+        group = group.split(",")
+        for set in group:           #Isolate each color
+            set = set.strip()
+            set = set.split(" ")
+            if set[1] == "red" and int(set[0]) > r:
+                r = int(set[0])
+            if set[1] == "green" and int(set[0]) > g:
+                g = int(set[0])
+            if set[1] == "blue" and int(set[0]) > b:
+                b = int(set[0])    
+    total = total + (r * g * b) 
+print(total)
+    
