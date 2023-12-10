32014e1506aeaacc936bc109b421e725c26a3ab0
Ian Lessly <ian.lessly@gmail.com>
2023-12-09 22:46:54 -0600

    Day 9 Completed

--- Day 9/Part 1.py
+++ Day 9/Part 1.py
@@ -1,3 +1,48 @@
 #Advent of Code Day 9 Part 1
 
-#Test push from my home computer
+input = open(r"Day 9\input.txt", "r")
+inputlist = input.readlines()
+
+sequence = []
+for x in inputlist:
+    sequence.append(x.strip().split())
+
+def FindDifferences(nums, finalnums):
+    #print("Recieved", nums)
+    difflist = []
+    for y in range(len(nums)-1):
+        #print("First number:", nums[y])
+        #print("Second number:", nums[y+1])
+        diff = int(nums[y+1]) - int(nums[y])
+        #print("Difference:", diff)
+        difflist.append(diff)
+    #print("Differences:", difflist)
+    #print("Final diff in list:", difflist[-1])
+    finalnums.append(difflist[-1])
+
+    return difflist, finalnums
+    
+
+total = 0
+for x in sequence:
+    nonzero = True
+    #print("Starting sequence:", x)
+    finalnums = [int(x[-1])]
+    diffs, finalnums = FindDifferences(x, finalnums)
+    #print(diffs)
+    while nonzero:
+        #print("Non zero diffs! Finding next tier.")
+        diffs, finalnums = FindDifferences(diffs, finalnums)
+        #print(diffs, finalnums)
+        zeros = diffs.count(0)
+        if zeros == len(diffs):
+            nonzero = False
+            
+    #print("Done!", diffs)
+    #print("Final of each seq:", finalnums)
+    next = sum(finalnums)
+    #print("Next num is", next)
+    total += next
+    #print("Running total is:", total)
+print("Final total is:", total)
+        
--- Day 9/Part 2.py
+++ Day 9/Part 2.py
@@ -1,1 +1,53 @@
 #Advent of Code Day 9 Part 2
+
+input = open(r"Day 9\input.txt", "r")
+inputlist = input.readlines()
+
+sequence = []
+for x in inputlist:
+    sequence.append(x.strip().split())
+
+def FindDifferences(nums, firstnums):
+    #print("Recieved", nums)
+    difflist = []
+    for y in range(len(nums)-1):
+        #print("First number:", nums[y])
+        #print("Second number:", nums[y+1])
+        diff = int(nums[y+1]) - int(nums[y])
+        #print("Difference:", diff)
+        difflist.append(diff)
+    print("Differences:", difflist)
+    #print("Final diff in list:", difflist[-1])
+    firstnums.insert(0, difflist[0])
+
+    return difflist, firstnums
+    
+
+total = 0
+for x in sequence:
+    nonzero = True
+    #print("Starting sequence:", x)
+    firstnums = [int(x[0])]
+    diffs, firstnums = FindDifferences(x, firstnums)
+    #print(diffs)
+    while nonzero:
+        #print("Non zero diffs! Finding next tier.")
+        diffs, firstnums = FindDifferences(diffs, firstnums)
+        #print(diffs, finalnums)
+        zeros = diffs.count(0)
+        if zeros == len(diffs):
+            nonzero = False
+            
+    print("Done!", diffs)
+    print("First of each seq:", firstnums)
+    next = 0
+    for i in range(len(firstnums)):
+        #print("Subtracting", next, "from", firstnums[i])
+        next = firstnums[i] - next
+        #print(next)
+   
+    print("Next num is", next)
+    total += next
+    #print("Running total is:", total)
+print("Final total is:", total)
+        
