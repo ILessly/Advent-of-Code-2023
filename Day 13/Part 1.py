0becda3d3fc1b6c14b64283488999e1cc028f9ed
Ian Lessly <ian.lessly@gmail.com>
2023-12-16 15:17:33 -0600

    Day 13 part 1 done

--- Day 13/Part 1.py
+++ Day 13/Part 1.py
@@ -1,6 +1,6 @@
-#Advent of Code Day 12 Part 1
+#Advent of Code Day 13 Part 1
 
-input = open(r"Day 13/example.txt", "r")
+input = open(r"Day 13/input.txt", "r")
 inputlist = input.readlines()
 
 inputblocks = []
@@ -38,32 +38,69 @@ def PrintBlock(block):
     print("\n")
 
 def CheckRows(rows):
-  print("Checking rows...")
+  print("Checking rows...") if printrows == True else None
   for row in range(len(rows)-1):
-    x1 = rows[row]
-    x2 = rows[row+1]
+    row1 = row
+    row2 = row+1
+    x1 = rows[row1]
+    x2 = rows[row2]
     while x1 == x2:
-      print("Match found! Row:", row, "and", row+1)
+      print("Match found! Row:", row1, "and", row2) if printrows == True else None
+      row1 += -1 if row1 > 0 else row2 + 1000
+      row2 += 1
       try:
-        x1 = rows[rows.index(x1)-1]
-        x2 = rows[rows.index(x2)+1]
-        print("Checking row", rows.index(x1)-1, "and", rows.index(x2)+1, "...")
-
-      except:
-        print("Edge reached.")
-        break
-
+        x1 = rows[row1]
+        x2 = rows[row2]
+        print("Checking row", row1, "and", row2, "...") if printrows == True else None
         
-  
+        
+      except:
+        print("Edge found. Mirror confirmed! Between", row, "and", row+1) if printrows == True else None
+        return row+1
+
+  print("No row mirror found.") if printrows == True else None
+  return 0
+
 
 def CheckCols(cols):
-  print("Checking columns...")
+  print("Checking columns...") if printcols == True else None
+  for col in range(len(cols)-1):
+    col1 = col
+    col2 = col+1
+    x1 = cols[col1]
+    x2 = cols[col2]
+    while x1 == x2:
+      print("Match found! Column:", col1, "and", col2) if printcols == True else None
+      col1 += -1 if col1 > 0 else col2 + 1000
+      col2 += 1
+      try:
+        x1 = cols[col1]
+        x2 = cols[col2]
+        print("Checking column", col1, "and", col2, "...") if printcols == True else None
+        
+      except:
+        print("Edge found. Mirror confirmed! Between", col, "and", col+1) if printcols == True else None
+        return col+1
 
+  print("No column mirror found.") if printcols == True else None
+  return 0
+  
+  
+printrows = False
+printcols = False
+total = 0
 for block in blocks.keys():
-  print(block)
-  CheckRows(blocks.get(block)[0])
-  CheckCols(blocks.get(block)[1])
+  print("=================", block, "================================")
+  colvalue = CheckCols(blocks.get(block)[1]) 
+  rowvalue = CheckRows(blocks.get(block)[0])
+  if colvalue == 0 and rowvalue == 0:
+    print("Anomaly found, no mirror:", block)
+  if colvalue != 0 and rowvalue != 0:
+    print("Anomaly fouund, two mirrors:", block)
+  total += (100*rowvalue) + colvalue
   print("Done!")
+  
+print("All done! Total:", total)
 
 
 
